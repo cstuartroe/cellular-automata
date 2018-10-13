27c0acd64933b5dda7ffecdd2dbb3f4d16e97372
Conor <cstuartroe@haverford.edu>
2018-10-12 22:50:43 -0400

    looks pretty sweet

--- cellular_automata.py
+++ cellular_automata.py
@@ -1,4 +1,5 @@
-from math import pi, sin, cos
+from math import pi, sin, cos, log2
+import random
  
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
@@ -7,11 +8,19 @@ from direct.interval.IntervalGlobal import Sequence
 from panda3d.core import Point3, PointLight, VBase4, NodePath, Geom, GeomVertexWriter, GeomVertexFormat, GeomVertexData, GeomTriangles, GeomNode
 
 SR2 = 2**.5
-NUM_ROWS = 10
+NUM_ROWS = 20
+HALFLIFE = 100
 
-class Rhomdo(GeomNode):
-    def __init__(self,x,y,z):
-        super(Rhomdo,self).__init__("rhomdo")
+class Rhomdo(NodePath):
+    def __init__(self,x,y,z,array):
+        self.geomnode = GeomNode("rhomdo")
+        super(Rhomdo,self).__init__(self.geomnode)
+        self.x, self.y, self.z = x,y,z
+        self.id = x*(NUM_ROWS**2) + y*NUM_ROWS + z
+        self.array = array
+        self.color = (x/NUM_ROWS),(y/NUM_ROWS),(z/NUM_ROWS),1
+        self.lifespan = -HALFLIFE*log2(1-random.random())
+        self.hidden = False
         
         format = GeomVertexFormat.getV3c4() 
         vdata = GeomVertexData("vertices", format, Geom.UHStatic) 
@@ -21,30 +30,33 @@ class Rhomdo(GeomNode):
         colorWriter = GeomVertexWriter(vdata, "color")
 
         for i in range(14):            
-            colorWriter.addData4f((x/NUM_ROWS),(y/NUM_ROWS),(z/NUM_ROWS),1)
+            colorWriter.addData4f(*self.color)
 
-        if z%2 == 1:
-            x += .5
-            y += .5
+        realX = self.x*2 - NUM_ROWS
+        realY = self.y*2 - NUM_ROWS
+        realZ = self.z*SR2 - SR2*.5*NUM_ROWS
+        if self.z%2 == 1:
+            realX += .5
+            realY += .5
         
-        vertexWriter.addData3f((2*x),   (2*y),   SR2*(z+1))
+        vertexWriter.addData3f(realX,   realY,   realZ+SR2)
         
-        vertexWriter.addData3f((2*x)-1, (2*y),   SR2*(z+.5))
-        vertexWriter.addData3f((2*x),   (2*y)-1, SR2*(z+.5))
-        vertexWriter.addData3f((2*x)+1, (2*y),   SR2*(z+.5))
-        vertexWriter.addData3f((2*x),   (2*y)+1, SR2*(z+.5))
+        vertexWriter.addData3f(realX-1, realY,   realZ+(SR2/2))
+        vertexWriter.addData3f(realX,   realY-1, realZ+(SR2/2))
+        vertexWriter.addData3f(realX+1, realY,   realZ+(SR2/2))
+        vertexWriter.addData3f(realX,   realY+1, realZ+(SR2/2))
 
-        vertexWriter.addData3f((2*x)-1, (2*y)-1, SR2*(z))
-        vertexWriter.addData3f((2*x)+1, (2*y)-1, SR2*(z))
-        vertexWriter.addData3f((2*x)+1, (2*y)+1, SR2*(z))
-        vertexWriter.addData3f((2*x)-1, (2*y)+1, SR2*(z))
+        vertexWriter.addData3f(realX-1, realY-1, realZ)
+        vertexWriter.addData3f(realX+1, realY-1, realZ)
+        vertexWriter.addData3f(realX+1, realY+1, realZ)
+        vertexWriter.addData3f(realX-1, realY+1, realZ)
         
-        vertexWriter.addData3f((2*x)-1, (2*y),   SR2*(z-.5))
-        vertexWriter.addData3f((2*x),   (2*y)-1, SR2*(z-.5))
-        vertexWriter.addData3f((2*x)+1, (2*y),   SR2*(z-.5))
-        vertexWriter.addData3f((2*x),   (2*y)+1, SR2*(z-.5))
+        vertexWriter.addData3f(realX-1, realY,   realZ-(SR2/2))
+        vertexWriter.addData3f(realX,   realY-1, realZ-(SR2/2))
+        vertexWriter.addData3f(realX+1, realY,   realZ-(SR2/2))
+        vertexWriter.addData3f(realX,   realY+1, realZ-(SR2/2))
         
-        vertexWriter.addData3f((2*x),   (2*y),   SR2*(z-1))
+        vertexWriter.addData3f(realX,   realY,   realZ-SR2)
         
         #step 2) make primitives and assign vertices to them 
         tris=GeomTriangles(Geom.UHStatic) 
@@ -154,30 +166,44 @@ class Rhomdo(GeomNode):
         tris.closePrimitive()
 
         #step 3) make a Geom object to hold the primitives 
-        cubeGeom=Geom(vdata) 
-        cubeGeom.addPrimitive(tris)
-        print(cubeGeom)
+        rhomGeom=Geom(vdata) 
+        rhomGeom.addPrimitive(tris)
         #now put squareGeom in a GeomNode. You can now position your geometry in the scene graph. 
-        self.addGeom(cubeGeom) 
+        self.geomnode.addGeom(rhomGeom)
+
+    def update(self,time):
+        if time > self.lifespan and not self.hidden:
+            self.hide()
+            self.hidden = True
 
 class MyApp(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
 
+        self.rhomdos = []
+
         for x in range(NUM_ROWS):
+            rhomdosY = []
             for y in range(NUM_ROWS):
+                rhomdosZ = []
                 for z in range(NUM_ROWS):
-                    render.attachNewNode(Rhomdo(x,y,z))
+                    rhomdo = Rhomdo(x,y,z,self.rhomdos)
+                    render.attachNewNode(rhomdo.geomnode)
+                    rhomdosZ.append(rhomdo)
+                rhomdosY.append(rhomdosZ)
+            self.rhomdos.append(rhomdosY)
+
+        print("Rhomdos loaded.")
 
         plight = PointLight('plight')
         plight.setColor(VBase4(1, 1, 1, 1))
-        plight.setAttenuation((0,0,.001))
+        plight.setAttenuation((0,0,.0005))
         self.plnp = render.attachNewNode(plight)
-        self.plnp.setPos(NUM_ROWS,NUM_ROWS,(NUM_ROWS*SR2)+40)
+        self.plnp.setPos(0,0,(NUM_ROWS*SR2)+40)
         render.setLight(self.plnp)
  
         # Disable the camera trackball controls.
-        #self.disableMouse()
+        self.disableMouse()
  
         # Load the environment model.
         #self.scene = NodePath()
@@ -187,18 +213,32 @@ class MyApp(ShowBase):
         #self.scene.setColor(.9,.4,.7,1)
  
         # Add the spinCameraTask procedure to the task manager.
-        #self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
-
-        
-        self.camera.setPos(-(NUM_ROWS), -(NUM_ROWS), 0)
-        self.camera.setHpr(0, 0, 0)
+        self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
+        self.taskMgr.add(self.updateClock, "UpdateClock")
+        self.clock = 0
  
     # Define a procedure to move the camera.
     def spinCameraTask(self, task):
         angleDegrees = task.time * 6.0
         angleRadians = angleDegrees * (pi / 180.0)
-        self.camera.setPos(30 * sin(angleRadians), -30.0 * cos(angleRadians), 20)
+        self.camera.setPos(100 * sin(angleRadians), -100.0 * cos(angleRadians), 50)
         self.camera.setHpr(angleDegrees, -30, 0)
+        return Task.cont
+
+    def updateRhomdos(self):
+        for x in range(NUM_ROWS):
+            for y in range(NUM_ROWS):
+                for z in range(NUM_ROWS):
+                    #print(self.rhomdos[x][y][z].id)
+                    self.rhomdos[x][y][z].update(self.clock)
+        return Task.cont
+
+    def updateClock(self, task):
+        adjusted_time = task.time * 20
+        if round(adjusted_time) > self.clock:
+            self.clock = round(adjusted_time)
+            print(self.clock)
+            self.updateRhomdos()
         return Task.cont
  
 app = MyApp()
