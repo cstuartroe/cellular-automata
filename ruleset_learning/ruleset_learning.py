370605715835a097b76bfe7528e87ba609822179
kristian1108 <krlgaylord@gmail.com>
2020-01-15 00:38:04 -0800

    sparse machine learning done

--- ruleset_learning/ruleset_learning.py
+++ ruleset_learning/ruleset_learning.py
@@ -2,6 +2,10 @@ import numpy as np
 import random
 import operator
 from tqdm import tqdm
+import tensorflow as tf
+from tensorflow.keras import models, layers
+import datetime
+import time
 
 
 class RulesetLearner:
@@ -12,6 +16,10 @@ class RulesetLearner:
         self.game_kwargs = game_kwargs
         self.num_frames = num_frames
         self.num_trials = num_trials
+        self.num_layers = 6
+        self.packed_samples = np.zeros((100, self.game_class.RULE_SPEC.num_dimensions))
+        self.packed_labels = np.zeros((100, 1))
+        self.packed_count = 0
 
     def monte_ruleset(self, rulevector):
         ruleargs, rulekwargs = self.game_class.rulevector2args(rulevector)
@@ -24,8 +32,205 @@ class RulesetLearner:
 
         return sum(outputs)/len(outputs)
 
-    def careful_explore(self, known_states, epsilon=0.3, steps=10, verbose=False, write_to_file='tests/rule_set.txt'):
+    def deep_explore(self, epsilon, lr=0.005, grad_step_scalar=10, init_on_first=True,
+                     num_best=20, save_to='trained_model.h5', epsilon_increment=0.01,
+                     train_samples=1000, validation_samples=100, cut_off_increment=0.05):
 
+        its = abs((epsilon - 1)/epsilon_increment)
+        step = 1
+
+        print("Based on your parameters, explore will run approximately " + str(its) + " times.")
+        time.sleep(5)
+        best = []
+
+        if init_on_first:
+            print('Training initial model...')
+            self.train_suggestion_model(lr=lr, train_samples=train_samples,
+                                        validation_samples=validation_samples, save_to=save_to)
+
+        while epsilon <= 1:
+            print('Starting step ' + str(step))
+            step += 1
+            new_test = self.game_class.RULE_SPEC.generate()
+
+            cut_off = random.uniform(0,1)
+
+            while cut_off < epsilon:
+                gs = abs(epsilon-cut_off)
+                print('Fetching gradient descent step...')
+                new_test, og_loss, new_loss = self.deep_suggestion(ruleset=new_test, new_training=False,
+                                                lr=lr, grad_step_scalar=(grad_step_scalar*gs), save_to=save_to)
+                cut_off += cut_off_increment
+
+            result = self.monte_ruleset(new_test)
+            epsilon += epsilon_increment
+
+            print("Pushing to continued training...")
+            self.continue_training(new_test, result, load_from=save_to)
+
+            if len(best) < num_best:
+                best.append((new_test, result))
+            else:
+                min_of_best = min(best, key = lambda t: t[1])
+                if result > min_of_best[1]:
+                    print('Deleting value...')
+                    best.sort(key=operator.itemgetter(1))
+                    del best[0]
+                    best.append((new_test, result))
+
+        best.sort(key=operator.itemgetter(1))
+
+        return best
+
+    def deep_suggestion(self, ruleset, new_training=False, lr=0.005,
+                        train_samples=1000, validation_samples=100, save_to='trained_model.h5',
+                        load_from='trained_model.h5', target_value=1, grad_step_scalar=10):
+
+        if new_training:
+            self.train_suggestion_model(lr=lr, train_samples=train_samples,
+                                        validation_samples=validation_samples, save_to=save_to)
+
+            load_from = save_to
+
+        model = tf.keras.models.load_model(load_from)
+        ruleset_tensor = self.ruleset_to_tensor(ruleset)
+
+        loss_func = tf.losses.MeanAbsoluteError()
+
+        og_loss = loss_func(target_value, model(ruleset_tensor))
+
+        with tf.GradientTape(persistent=True) as gt:
+            result = model(ruleset_tensor)
+            loss = loss_func(target_value, result)
+
+        gradients = gt.gradient(loss, model.trainable_weights)
+
+        sans_bias = [grad for grad in gradients if len(grad.shape) == 2]
+
+        loss_over_in = sans_bias[0]
+
+        for g in sans_bias[1:]:
+            loss_over_in = tf.linalg.matmul(loss_over_in, g)
+
+        input_gradient = loss_over_in.numpy()
+        input_gradient = np.reshape(input_gradient, (self.game_class.RULE_SPEC.num_dimensions,))
+
+        suggestion = ruleset + (input_gradient * grad_step_scalar)
+
+        valid = self.is_valid_ruleset(suggestion)
+
+        if isinstance(valid, list):
+            for i in valid:
+                dimensions = self.game_class.RULE_SPEC.dimensions
+                if suggestion[i] < dimensions[i].start:
+                    suggestion[i] = dimensions[i].start
+                else:
+                    suggestion[i] = dimensions[i].end
+
+        new_loss = loss_func(target_value, model(self.ruleset_to_tensor(suggestion)))
+
+        return suggestion, og_loss, new_loss
+
+    def is_valid_ruleset(self, ruleset):
+        dimensions = self.game_class.RULE_SPEC.dimensions
+        faults = []
+
+        for i in range(len(dimensions)):
+            if ruleset[i] < dimensions[i].start or ruleset[i] > dimensions[i].end:
+                faults.append(i)
+
+        if not faults:
+            return True
+        else:
+            return faults
+
+    def ruleset_to_tensor(self, ruleset):
+
+        num_dim= self.game_class.RULE_SPEC.num_dimensions
+        rst = tf.convert_to_tensor(ruleset)
+        rst = tf.reshape(rst, (1, num_dim))
+
+        return rst
+
+    def train_suggestion_model(self, lr=0.005, train_samples=1000, validation_samples=100, save_to='trained_model.h5'):
+
+        dim = self.game_class.RULE_SPEC.num_dimensions
+
+        model = models.Sequential()
+
+        model.add(layers.Dense(64, activation='sigmoid', input_shape=(dim, )))
+        model.add(layers.Dense(32, activation='sigmoid', use_bias=True))
+
+        model.add(layers.Dense(1, activation='sigmoid', use_bias=True))
+
+        opt = tf.optimizers.Adam(learning_rate=lr)
+        met = tf.keras.metrics.MeanAbsoluteError()
+        loss = tf.losses.MeanAbsoluteError()
+
+        model.compile(optimizer=opt, loss=loss, metrics=[met])
+        model.summary()
+
+        print('Fetching training data...')
+        train_data = self.get_samples(train_samples, verbose=True)
+        print('Fetching validation data...')
+        validation_data = self.get_samples(validation_samples, verbose=True)
+
+        log_dir = "training_logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+
+        tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+        model.fit(train_data, epochs=10, verbose=1, validation_data=validation_data, callbacks=[tensorboard_callback])
+
+        model.save(save_to)
+
+    def get_samples(self, num=1000, verbose=False, round_to=3):
+
+        samples = np.zeros((num, self.game_class.RULE_SPEC.num_dimensions))
+        labels = np.zeros((num, 1))
+
+        if verbose:
+            iterator = tqdm(range(num))
+        else:
+            iterator = range(num)
+
+        for i in iterator:
+            rules = self.game_class.RULE_SPEC.generate()
+            value = self.monte_ruleset(rules)
+
+            samples[i] = rules
+            value = round(value, round_to)
+            labels[i] = value
+
+            data = tf.data.Dataset.from_tensor_slices((samples, labels))
+
+        return data.map(self.shape_tensor)
+
+    def continue_training(self, ruleset, value, load_from='trained_model.h5'):
+
+        if self.packed_count == 100:
+            print('Triggered new training.')
+            data = tf.data.Dataset.from_tensor_slices((self.packed_samples, self.packed_labels))
+            self.packed_count = 0
+            data = data.map(self.shape_tensor)
+            print('Loading model...')
+
+            model = tf.keras.models.load_model(load_from)
+
+            model.fit(data, epochs=5, verbose=1)
+            model.save(load_from)
+
+        else:
+            self.packed_samples[self.packed_count] = ruleset
+            self.packed_labels[self.packed_count] = value
+
+        self.packed_count += 1
+
+    def shape_tensor(self, sample, label):
+        sample = tf.reshape(sample, (1, self.game_class.RULE_SPEC.num_dimensions))
+        return sample, label
+
+    def careful_explore(self, known_states, epsilon=0.3, steps=10,
+                        verbose=False, write_to_file='tests/rule_set.txt', ep_decay_rate=0):
         dimensions = self.game_class.RULE_SPEC.dimensions
 
         iterator = range(steps)
@@ -37,11 +242,11 @@ class RulesetLearner:
             print('Now exploring...')
 
         for i in iterator:
+            epsilon -= ep_decay_rate
+
             if random.uniform(0, 1) < epsilon:
                 action = self.game_class.RULE_SPEC.generate()
-
             else:
-
                 try:
                     action = np.asarray(max(known_states.items(), key=operator.itemgetter(1))[0])
                 except ValueError:
@@ -55,8 +260,10 @@ class RulesetLearner:
                 elif dimensions[change].dtype == 'integer':
                     action[change] = random.randint(dimensions[change].start, dimensions[change].end)
                 else:
-                    while action[change] < dimensions[change].start or action[change] > dimensions[change].end:
+                    first = True
+                    while action[change] < dimensions[change].start or action[change] > dimensions[change].end or first:
                         action[change] += random.uniform(-1,1)
+                        first = False
 
             reward = self.monte_ruleset(action)
             act_tup = tuple(action)
